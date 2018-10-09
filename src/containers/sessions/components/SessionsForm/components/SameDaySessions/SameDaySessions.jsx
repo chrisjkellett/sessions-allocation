a33244982e780816faf5aaad3a5c201d2a0fe45d
chrisjkellett <cjkellett@hotmail.co.uk>
2018-10-09 12:11:52 +0200

    added pairing to same day sessions

--- src/containers/sessions/components/SessionsForm/components/SameDaySessions/SameDaySessions.jsx
+++ src/containers/sessions/components/SessionsForm/components/SameDaySessions/SameDaySessions.jsx
@@ -14,7 +14,7 @@ const SameDaySessions = ({ data }) => {
       {data.map(s => (
         <Tr key={s.id} >
           <TdIcons array={s.levels} subContent={<SubTd data={[s.type, s.venue, s.time]} inline/>} />
-          <Td data={s.examiners} />
+          <Td data={s.examiners} type={s.type}/>
           <Td data={s.support} />
           <td></td>
         </Tr>
