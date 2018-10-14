0a6c04d84e7a8e8b51827d937d78272f140d061f
chrisjkellett <cjkellett@hotmail.co.uk>
2018-10-14 23:48:47 +0200

    started styling for single view

--- src/containers/sessions/components/SessionsTable/components/SingleSession/SingleSession.jsx
+++ src/containers/sessions/components/SessionsTable/components/SingleSession/SingleSession.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { SingleView } from '../../../../../../components';
-
+import { SingleView, FlexContainer, FlexItem } from '../../../../../../components';
+import classes from './SingleSession.css';
 
 const SingleSession = ({ session }) => {
   return (
     <SingleView>
-      <div>{session.id}</div>
+      <FlexContainer>
+        <FlexItem>
+          <div className={classes.SingleItem}>
+            <span>session date</span>
+            <span>{session.session_date}</span>
+          </div>
+          <div>
+            <span>start time</span>
+            <span>{session.time}</span>
+          </div>
+        </FlexItem>
+      </FlexContainer>
     </SingleView>
   )
 }
