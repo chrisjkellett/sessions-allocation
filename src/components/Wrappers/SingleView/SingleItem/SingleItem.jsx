42d199f13c211b549c76d78fd4a98634e07d9ff7
chrisjkellett <cjkellett@hotmail.co.uk>
2018-10-16 17:47:12 +0200

    examiner dob integrated

--- src/components/Wrappers/SingleView/SingleItem/SingleItem.jsx
+++ src/components/Wrappers/SingleView/SingleItem/SingleItem.jsx
@@ -3,17 +3,19 @@ import classes from './SingleItem.css';
 import IconClasses from '../../../Tables/Td/TdIcons/TdIcons.css';
 import moment from 'moment';
 
-const SingleItem = ({ label, data, icons, isDate, array }) => {
-  const content = isDate ?  moment(data, 'YYYYMMDD').format('dddd Do MMMM YYYY') : data;
+const SingleItem = ({ label, data, icons, isDate, array, shortDate }) => {
+  const dateFormat = shortDate ? 'Do MMMM YYYY' : 'dddd Do MMMM YYYY'
+  const content = isDate ?  data && moment(data, 'YYYYMMDD').format(dateFormat) : data;
+  
   return (
     <div className={classes.SingleItem}>
       <div className={classes.Label}>{label}</div>
       {icons && 
-        <div className={classes.Data}>{content.map(item => <span className={IconClasses.Icons}>{item}</span>)}</div>}
+        <div className={classes.Data}>{content ? content.map(item => <span className={IconClasses.Icons}>{item}</span>) : '-'}</div>}
       {array && 
-        <div className={classes.Data}>{content.map(item => <div className={classes.Sub}>{item}</div>)}</div>}
+        <div className={classes.Data}>{content ? content.map(item => <div className={classes.Sub}>{item}</div>) : '-'}</div>}
       {!icons && !array && 
-        <div className={classes.Data}>{content}</div>}
+        <div className={classes.Data}>{content ? content : '-'}</div>}
     </div>
   )
 }
