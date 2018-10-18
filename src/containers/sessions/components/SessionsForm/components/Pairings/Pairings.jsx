eaed2cc60abb93bea3721fa7b28d294ef2a48902
chrisjkellett <cjkellett@hotmail.co.uk>
2018-10-18 14:35:24 +0200

    can select paired examiner

--- src/containers/sessions/components/SessionsForm/components/Pairings/Pairings.jsx
+++ src/containers/sessions/components/SessionsForm/components/Pairings/Pairings.jsx
@@ -2,15 +2,44 @@ import React, { Component } from 'react';
 import classes from './Pairings.css';
 
 class Pairings extends Component {
+  state = {
+    toPair: null,
+    examiners: null,
+  }
+
+  componentDidMount(){
+    this.setState({
+      examiners: this.props.examiners
+    })
+  }
+
+  change = (index) => {
+    const { toPair, examiners} = this.state;
+    if(toPair === null){
+      this.setState({ toPair: examiners[index]})
+    }
+  }
+
   render(){
-    const {examiners, handler} = this.props;
+    const {examiners, toPair} = this.state;
     return (
-      examiners.map((item, index) => index % 2 === 0 && 
-        <div key={index} id={index} className={classes.ExaminerPair}>
-          <span className={classes.Single} key={item.name} onClick={() => handler(index)}>{item.name}</span>
-          <span>+</span>
-          <span className={classes.Single} key={examiners[index + 1]} onClick={() => handler(index + 1)}>{examiners[index + 1].name}</span>
-        </div>
+      examiners === null ? 'loading' : 
+        examiners.map((item, index) => index % 2 === 0 && 
+          <div key={index} id={index} className={classes.ExaminerPair}>
+            <span 
+              className={toPair && item.name === toPair.name ? classes.Selected : classes.Single} 
+              key={item.name} 
+              onClick={() => this.change(index)}>
+              {item.name}
+            </span>
+            <span>+</span>
+            <span 
+              className={toPair && examiners[index + 1] === toPair.name ? classes.Selected : classes.Single} 
+              key={examiners[index + 1]} 
+              onClick={() => this.change(index + 1)}>
+              {examiners[index + 1].name}
+            </span>
+          </div>
       )
     )
   }
