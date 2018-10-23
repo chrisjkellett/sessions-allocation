0d04a2ca308f077da7cb6d9a549b84cf2e56af76
chrisjkellett <cjkellett@hotmail.co.uk>
2018-10-23 16:06:34 +0200

    updated classes for supervisors

--- src/components/Tables/Td/TdExaminer/TdExaminer.jsx
+++ src/components/Tables/Td/TdExaminer/TdExaminer.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import classes from '../Td/Td.css';
 
 const Td = ({ data, type, isYLE }) => {
-  console.log(data);
   let styles = [classes.ExaminerPair];
   
   const isWritingOrYLESpeaking = type === 'Writing' || (type === 'Speaking' && isYLE)
@@ -10,13 +9,14 @@ const Td = ({ data, type, isYLE }) => {
   return isWritingOrYLESpeaking 
     ? <td>
       {data.filter(i => i.supervisor).map(item => <div className={classes.ExaminerPair}>
-        <span>{item.name}</span>
+        <span className={classes.Thicker}>{item.name}</span>
         <span>{item.supervisor && ' (supervisor)'}</span>
       </div>)}
       {data.filter(i => !i.supervisor).map(item => <div className={classes.ExaminerPair}>
         <span>{item.name}</span>
       </div>)}
       </td>
+
     : <td className={styles.join(" ")}>
         {data.map((item, index) => index % 2 === 0 && 
           <div key={index} id={index} className={classes.ExaminerPair}>
