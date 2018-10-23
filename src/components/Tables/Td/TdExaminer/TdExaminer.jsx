5022c36af5599d4e21494baf973da3f8ef101143
chrisjkellett <cjkellett@hotmail.co.uk>
2018-10-23 16:03:55 +0200

    can select and display supervisors

--- src/components/Tables/Td/TdExaminer/TdExaminer.jsx
+++ src/components/Tables/Td/TdExaminer/TdExaminer.jsx
@@ -2,21 +2,29 @@ import React from 'react';
 import classes from '../Td/Td.css';
 
 const Td = ({ data, type, isYLE }) => {
-  console.log(data[0]);
+  console.log(data);
   let styles = [classes.ExaminerPair];
   
-  // const isWritingOrYLESpeaking = type === 'Writing' || (type === 'Speaking' && isYLE)
+  const isWritingOrYLESpeaking = type === 'Writing' || (type === 'Speaking' && isYLE)
 
-  return (
-    <td className={styles.join(" ")}>
-      {data.map((item, index) => index % 2 === 0 && 
-        <div key={index} id={index} className={classes.ExaminerPair}>
-          <span>{item.name} + </span>
-          {data[index + 1] && <span>{data[index + 1].name}</span>}
-        </div>
-      )}
-    </td>
-  )
+  return isWritingOrYLESpeaking 
+    ? <td>
+      {data.filter(i => i.supervisor).map(item => <div className={classes.ExaminerPair}>
+        <span>{item.name}</span>
+        <span>{item.supervisor && ' (supervisor)'}</span>
+      </div>)}
+      {data.filter(i => !i.supervisor).map(item => <div className={classes.ExaminerPair}>
+        <span>{item.name}</span>
+      </div>)}
+      </td>
+    : <td className={styles.join(" ")}>
+        {data.map((item, index) => index % 2 === 0 && 
+          <div key={index} id={index} className={classes.ExaminerPair}>
+            <span>{item.name} + </span>
+            {data[index + 1] && <span>{data[index + 1].name}</span>}
+          </div>
+        )}
+      </td>
 }
 
 export default Td;
