2c28d7c44c975fbeb8708eab7353fab7d0e6790d
chrisjkellett <cjkellett@hotmail.co.uk>
2018-10-25 15:32:08 +0200

    focuses on filter for examiners on mount

--- src/containers/examiners/components/ExaminersTable/ExaminersTable.jsx
+++ src/containers/examiners/components/ExaminersTable/ExaminersTable.jsx
@@ -1,33 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Table, Tr, Td, SubTd, TdIcons, TdIconsForTime, EditDeletePanel } from '../../../../components';
 import IsNotEmpty from '../../../../components/Wrappers/IsNotEmpty/IsNotEmpty';
 import Filter from '../../../../components/Filter/Filter';
 
-const ExaminersTable = ({ data, filtered, handlers, isConfirming, activeFilter }) => {
-  const examiners = filtered === null ? data : filtered;
-  const labels = ([
-    <Filter label='name' filter={handlers.filter} value={!activeFilter ? '' : undefined} />, 
-    null, 
-    <Filter label='levels' filter={handlers.filter} value={!activeFilter ? '' : undefined} />, 
-    <Filter label='availability' filter={handlers.filter} value={!activeFilter ? '' : undefined} />, 
-    null
-  ]);
+class ExaminersTable extends Component {
+  componentDidMount(){
+    document.getElementById('name').focus();
+  }
 
-  return (
-    <Table labels={labels}>
-      <IsNotEmpty data={examiners}>
-        {examiners.map(e => (
-          <Tr key={e.name}>
-            <Td data={e.name} subContent={<SubTd data={e.roles} />} handler={() => handlers.openSingleView(e.id)}  />
-            <td></td>
-            <TdIcons array={e.levels} />
-            <TdIconsForTime array={e.availability} />
-            <EditDeletePanel handlers={handlers} data={e} isConfirming={isConfirming} />
-          </Tr>
-        ))}
-      </IsNotEmpty>
-    </Table>
-  );
-};
+  render(){
+    const { data, filtered, handlers, isConfirming, activeFilter } = this.props;
+    const examiners = filtered === null ? data : filtered;
+    const labels = ([
+      <Filter label='name' filter={handlers.filter} value={!activeFilter ? '' : undefined} />, 
+      null, 
+      <Filter label='levels' filter={handlers.filter} value={!activeFilter ? '' : undefined} />, 
+      <Filter label='availability' filter={handlers.filter} value={!activeFilter ? '' : undefined} />, 
+      null
+    ]);
+  
+    return (
+      <Table labels={labels}>
+        <IsNotEmpty data={examiners}>
+          {examiners.map(e => (
+            <Tr key={e.name}>
+              <Td data={e.name} subContent={<SubTd data={e.roles} />} handler={() => handlers.openSingleView(e.id)}  />
+              <td></td>
+              <TdIcons array={e.levels} />
+              <TdIconsForTime array={e.availability} />
+              <EditDeletePanel handlers={handlers} data={e} isConfirming={isConfirming} />
+            </Tr>
+          ))}
+        </IsNotEmpty>
+      </Table>
+    );
+  }
+}
 
 export default ExaminersTable;
