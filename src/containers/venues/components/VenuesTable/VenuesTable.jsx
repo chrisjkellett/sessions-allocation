9113fbb6e87369814f0d3e918eb9eca3f5ce4125
chrisjkellett <cjkellett@hotmail.co.uk>
2018-10-25 15:35:09 +0200

    same for venues

--- src/containers/venues/components/VenuesTable/VenuesTable.jsx
+++ src/containers/venues/components/VenuesTable/VenuesTable.jsx
@@ -1,33 +1,38 @@
-import React from 'react';
-import { Table, Td, Tr, SubTd, EditDeletePanel } from '../../../../components';
-import Filter from '../../../../components/Filter/Filter';
-import IsNotEmpty from '../../../../components/Wrappers/IsNotEmpty/IsNotEmpty';
+import React, { Component } from 'react';
+import { Table, Td, Tr, SubTd, EditDeletePanel, Filter, IsNotEmpty } from '../../../../components';
 
-const VenuesTable = ({ data, filtered, handlers, isConfirming }) => {
-  const venues = filtered === null ? data : filtered;
-  const labels = ([
-    <Filter label='name' filter={handlers.filter} />, 
-    null,
-    'contact', 
-    <Filter label='type' filter={handlers.filter} />, 
-    null
-  ]);
+class VenuesTable extends Component {
+  componentDidMount(){
+    document.getElementById('name').focus();
+  }
 
-  return (
-    <Table labels={labels}>
-      <IsNotEmpty data={venues}>
-        {venues.map(v => (
-          <Tr key={v.name}>
-            <Td data={v.name} subContent={<SubTd data={v.address} inline />} />
-            <td></td>
-            <Td data={v.contact} />
-            <Td data={v.type} type='array'/>
-            <EditDeletePanel handlers={handlers} data={v} isConfirming={isConfirming} />
-          </Tr>
-        ))}
-      </IsNotEmpty>
-    </Table>
-  );
-};
+  render(){
+    const { data, filtered, handlers, isConfirming } = this.props;
+    const venues = filtered === null ? data : filtered;
+    const labels = ([
+      data.length !== 0 && <Filter label='name' filter={handlers.filter} />, 
+      null,
+      data.length !== 0 && 'contact', 
+      data.length !== 0 && <Filter label='type' filter={handlers.filter} />, 
+      null
+    ]);
+  
+    return (
+      <Table labels={labels}>
+        <IsNotEmpty data={venues}>
+          {venues.map(v => (
+            <Tr key={v.name}>
+              <Td data={v.name} subContent={<SubTd data={v.address} inline />} />
+              <td></td>
+              <Td data={v.contact} />
+              <Td data={v.type} type='array'/>
+              <EditDeletePanel handlers={handlers} data={v} isConfirming={isConfirming} />
+            </Tr>
+          ))}
+        </IsNotEmpty>
+      </Table>
+    );
+  }
+}
 
 export default VenuesTable;
