5d803938c21222c0151a3c3c57b93ff0a1f850a0
chrisjkellett <cjkellett@hotmail.co.uk>
2018-10-26 18:06:49 +0200

    added kick ass background

--- src/components/AsyncLoad/AsyncLoad.js
+++ src/components/AsyncLoad/AsyncLoad.js
@@ -7,7 +7,7 @@ const AsyncLoad = ({ children, waitFor, error }) => {
     return (
       <div className={classes.Error}>
         <i class="fas fa-exclamation-triangle"></i>
-        <span>An error has occured while {error.action} {error.type} from database</span>
+        <span>An error has occured while {error.action} {error.type} from database - try refreshing the page or contact support</span>
       </div>
     )
   else if(waitFor === null)
--- src/containers/auth/renders/index.js
+++ src/containers/auth/renders/index.js
@@ -8,18 +8,20 @@ import classes from '../Auth.css';
 
 export const renderUI = (state, inputHandler, submitHandler, error, showErrors) => {
     return(
-      <section className={classes.Auth}>
-        <Logo styles='Centre-Mid'/>
-        {error && showErrors && renderError(error)}
-        <form onSubmit={submitHandler}>
-          {generateFormElementArray(state)
-            .map(element =>{
-              return <Input {...generateInputProps(element, state, inputHandler)} />
-            }
-          )}
-          <button>login</button>
-          {/* {renderError(error)} */}
-        </form>
-      </section>
+      <div>
+        <section className={classes.Auth}>
+          <Logo styles='Centre-Mid'/>
+          {error && showErrors && renderError(error)}
+          <form onSubmit={submitHandler}>
+            {generateFormElementArray(state)
+              .map(element =>{
+                return <Input {...generateInputProps(element, state, inputHandler)} />
+              }
+            )}
+            <button>login</button>
+            {/* {renderError(error)} */}
+          </form>
+        </section>
+      </div>
     )
 }
