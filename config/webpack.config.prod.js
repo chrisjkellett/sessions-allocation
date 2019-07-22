1b0b6d99545bdbf8e98539143a0fc3a1d9e72893
chrisjkellett <cjkellett@hotmail.co.uk>
2019-07-22 14:34:03 +0200

    updated vulnerable pkg css loader and edited webpack config removing invalid option

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -183,7 +183,7 @@ module.exports = {
                         modules: true,
                         localIdentName: '[name]__[local]__[hash:base64:5]',
                         importLoaders: 1,
-                        minimize: true,
+                        // minimize: true,
                         sourceMap: shouldUseSourceMap,
                       },
                     },
